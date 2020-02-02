ef2b225419aecfe86a454e698519dc38e2ab3b5d
Chris Antaki <ChrisAntaki@gmail.com>
2020-02-02 01:56:46 -0800

    Fixes hanging test

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const server = express();
-server.get('/', (req, res) => {
+const app = express();
+app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-server.listen(PORT, HOST);
+const server = app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
 // Exports
