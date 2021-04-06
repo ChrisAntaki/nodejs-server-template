1ed2b87e9f9697f806e0935fe3d600ce0d1afe5c
Chris Antaki <ChrisAntaki@gmail.com>
2021-04-05 21:29:26 -0700

    Removes Wasm (#11)
    * Remove WASM features.
    
    * Update yarn lock file.
    
    * Remove extra dep.
    
    * Remove Wasm build step.

--- assembly/README.md
+++
@@ -1,7 +0,0 @@
-# Wasm
-
-This directory contains AssemblyScript used for advanced computations. Here's how to compile it:
-
-```sh
-yarn build  # Builds Wasm module for server.
-```
--- test/server-test.js
+++ test/server-test.js
@@ -7,26 +7,13 @@ chai.use(chaiHttp);
 
 describe('Server', () => {
   describe('GET /', () => {
-    it('responds with message', done => {
+    it('responds with message', (done) => {
       chai
         .request(server)
         .get('/')
         .end((err, res) => {
           expect(res.status).to.equal(200);
           expect(res.text).to.equal('Hello World');
-          done();
-        });
-    });
-  });
-
-  describe('GET /add', () => {
-    it('responds with sum of two numbers', done => {
-      chai
-        .request(server)
-        .get('/add?a=2&b=2')
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.text).to.equal('4');
           done();
         });
     });
